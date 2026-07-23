0ce466eeac2dd935f9f4f82d048e8c5e991102d7
Gabrielle Furtado <gabriellerodriguesfurtado@gmail.com>
2026-07-22 21:08:16 -0300

    Atividade 09

---
+++ atividade08.php
@@ -0,0 +1,36 @@
+<?php
+/* Exercício 8 – Estoque
+A loja possui 20 produtos.
+● Venda: rand(1,4)
+● Enquanto houver estoque.
+● Conte as vendas.
+● Some os itens vendidos.
+*/
+
+$estoque = 20;
+$qtdVendas = 0;
+$vendidos = 0;
+
+while ($estoque > 0) {
+
+    $vendas = rand(1, 4);
+
+    if ($vendas <= $estoque) {
+
+        $estoque -= $vendas;
+        $vendidos += $vendas;
+        $qtdVendas++;
+
+        echo "Número de vendas: $qtdVendas <br>";
+        echo "Itens vendidos: $vendas <br>";
+        echo "Estoque restante: $estoque <br>";
+    } else {
+
+        echo "Não foi possível realizar a venda de $vendas itens, pois restam apenas $estoque em estoque.<br>";
+    }
+
+    echo "<hr>";
+}
+
+echo "<strong>Total de vendas: $qtdVendas</strong><br>";
+echo "<strong>Total de itens vendidos: $vendidos</strong><br>";
